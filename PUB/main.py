82e22a01f5aee8a52421747560eb43a7cdc4ea20
YeruhamMendelson <y0583207549@gmail.com>
2025-08-26 09:02:26 +0300

    commit before marge

--- PUB/main.py
+++ PUB/main.py
@@ -7,7 +7,6 @@ from kalfa_producer import Kalfa_producer
 
 
 SERVER = os.getenv("SERVER", "localhost:9092")
-PRODUCER = Kalfa_producer(SERVER)
 
 
 @asynccontextmanager
@@ -21,32 +20,28 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-
-
 @app.get('/')
 def root():
     return {"massage:": "api of producer news, by path /news it producer 20 news to two topics"}
-
 
 
 @app.get('/news')
 async def produces_news():
 
     tan_interesting = next(newsgroups_interesting)
-    print(tan_interesting)
     tan_not_interesting = next(newsgroups_not_interesting)
-    print(tan_not_interesting)
+
     PRODUCER.publish_messages('interesting' ,'massage' , tan_interesting)
     PRODUCER.publish_messages('not_interesting', 'massage', tan_not_interesting)
 
 
 
 
-
-
-
 if __name__ == "__main__":
+
+    PRODUCER = Kalfa_producer(SERVER)
     newsgroups_interesting = news.iter_data(news.get_newsgroups_interesting(), 10)
     newsgroups_not_interesting = news.iter_data(news.get_newsgroups_not_interesting(), 10)
+
     uvicorn.run(app, host="0.0.0.0", port=8081)
 
